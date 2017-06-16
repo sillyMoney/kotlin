a09ee43f34da7038dd51bd453c163098effa3756
sillyMoney <linxiaotian1986@gmail.com>
2017-06-16 21:34:40 +0800

    init project

---
+++ src/main/kotlin/com/yushan/finance/kotlintest/KotlinTestApplication.kt
@@ -0,0 +1,11 @@
+package com.yushan.finance.kotlintest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinTestApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinTestApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/yushan/finance/kotlintest/KotlinTestApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.yushan.finance.kotlintest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
